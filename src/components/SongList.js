6ae7d013e294164e7351c86f61d8417bf1f7ce93
johnpospisil <audiofreak7@gmail.com>
2019-05-30 15:06:07 -0700

    Call Action Creators from components. The 'Select' buttons are onw listening onClick.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
   renderList() {
@@ -7,7 +8,7 @@ class SongList extends React.Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button className="ui button primary" onClick={() => this.props.selectSong(song)}>Select</button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -23,9 +24,9 @@ class SongList extends React.Component {
 // This function has this name by convention.
 // take the state object, run some computation on the data to make it appear as props
 const mapStateToProps = (state) => {
-  // console.log(state);
+  console.log(state);
   return { songs: state.songs };
 }
 
 // the 'connect' function returns a function. We set 'SongList' as the input to that function.
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
